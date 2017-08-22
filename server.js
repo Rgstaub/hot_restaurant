61563704fc3a945d29405334970ba945cf40cc24
Taylor Caton <TaylorCaton@hgbc.org>
2017-08-22 19:10:07 -0400

    added npm packages

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var api = require('./app/routing/apiRoutes.js');
-var router = require('./app/routing/htmlRoutes.js');
+var api = require('./app/routes/apiRoutes.js');
+var router = require('./app/routes/htmlRoutes.js');
 
 
 // Sets up the Express App
@@ -12,17 +12,12 @@ var router = require('./app/routing/htmlRoutes.js');
 var app = express();
 var PORT = process.env.PORT || 3002;
 
-
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
-
-app.use("/static/", express.static(path.join(__dirname, "public")));
 app.use('/api', api);
 app.use('/', router);
 // Starts the server to begin listening
